4aeea4e4059fb474649fad16dc8ee617a9f0a187
arifudin <nanangarif404@gmail.com>
2020-11-04 21:39:10 +0700

    tambah bottom navigation

--- app/src/main/java/com/android/jawalur/MainActivity.kt
+++ app/src/main/java/com/android/jawalur/MainActivity.kt
@@ -1,26 +1,47 @@
 package com.android.jawalur
 
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.android.jawalur.ui.dashboard.DashboardFragment
+import com.android.jawalur.ui.favorite.FavoriteFragment
+import com.android.jawalur.ui.kuis.KuisFragment
 import com.android.jawalur.ui.login.LoginActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     val WEB_CLIENT = "564803799642-hd0oai62858tehgclcjmm1kjh8vo3lnj.apps.googleusercontent.com"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_keluar.setOnClickListener { keluar() }
+        loadFragment(DashboardFragment())
+        setBottomNavigationView()
 
+    }
+
+    private fun setBottomNavigationView() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bn_main)
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fl_container, fragment)
+                .addToBackStack(null)
+                .commit()
+            return true
+        }
+        return false
     }
 
     private fun keluar() {
@@ -40,6 +61,16 @@ class MainActivity : AppCompatActivity() {
                 this.finish()
             }
             .create().show()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var fragment: Fragment? = null
+        when (item.getItemId()) {
+            R.id.dashboard_menu -> fragment = DashboardFragment()
+            R.id.favorite_menu -> fragment = FavoriteFragment()
+            R.id.profile_menu -> fragment = KuisFragment()
+        }
+        return loadFragment(fragment)
     }
 
 }
