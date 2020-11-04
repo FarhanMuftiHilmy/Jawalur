4700e1de3602d97c0ccdfbbe138a5c0c4d57938c
arifudin <nanangarif404@gmail.com>
2020-11-04 17:32:06 +0700

    fitur daftar bisa, layout belum rapi

--- app/src/main/java/com/android/jawalur/ui/login/LoginActivity.kt
+++ app/src/main/java/com/android/jawalur/ui/login/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.android.jawalur.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.jawalur.R
+import com.android.jawalur.ui.register.RegisterActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_daftar.setOnClickListener { startActivity(Intent(this, RegisterActivity::class.java)) }
+
+
     }
 }
--- app/src/main/java/com/android/jawalur/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/android/jawalur/ui/register/RegisterActivity.kt
@@ -1,12 +1,63 @@
 package com.android.jawalur.ui.register
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.PendingIntent.getActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.jawalur.MainActivity
 import com.android.jawalur.R
+import com.android.jawalur.ui.login.LoginActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import kotlinx.android.synthetic.main.activity_register.*
+
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val TAG = "RegisterActivity"
+    private val mAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        btn_daftar.setOnClickListener {
+            registerUser()
+        }
+
+        btn_masuk.setOnClickListener { startActivity(Intent(this, LoginActivity::class.java)) }
     }
+
+    private fun registerUser() {
+        val email = edt_email_register.text.toString()
+        val password = edt_password_register.text.toString()
+        val name = edt_name_register.text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty()) {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this
+            ) { task ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    Toast.makeText(this, "Successfully registered :)", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Error registering, try again later :(",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }else {
+            Toast.makeText(this,"Please fill up the Credentials :|", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+
 }
