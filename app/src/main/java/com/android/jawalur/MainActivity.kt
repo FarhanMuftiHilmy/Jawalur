b5701821d590851a6d6b0a3f7a769643ca3af564
arifudin <nanangarif404@gmail.com>
2020-11-04 19:01:07 +0700

    fitur masuk dan keluar bisa, layout belum rapi

--- app/src/main/java/com/android/jawalur/MainActivity.kt
+++ app/src/main/java/com/android/jawalur/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.android.jawalur
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.jawalur.ui.login.LoginActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    val WEB_CLIENT = "564803799642-hd0oai62858tehgclcjmm1kjh8vo3lnj.apps.googleusercontent.com"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_keluar.setOnClickListener { keluar() }
+
     }
+
+    private fun keluar() {
+        val firebaseAuth = FirebaseAuth.getInstance()
+        AlertDialog.Builder(this)
+            .setMessage("Apakah Anda yakin ingin keluar?")
+            .setNegativeButton("Tidak", null)
+            .setNeutralButton("Batal", null)
+            .setPositiveButton("Ya") { dialogInterface, i ->
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(WEB_CLIENT)
+                    .requestEmail()
+                    .build()
+                GoogleSignIn.getClient(this, gso).signOut()
+                firebaseAuth.signOut()
+                startActivity(Intent(this, LoginActivity::class.java))
+                this.finish()
+            }
+            .create().show()
+    }
+
 }
--- app/src/main/java/com/android/jawalur/ui/login/LoginActivity.kt
+++ app/src/main/java/com/android/jawalur/ui/login/LoginActivity.kt
@@ -1,13 +1,28 @@
 package com.android.jawalur.ui.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.android.jawalur.MainActivity
 import com.android.jawalur.R
 import com.android.jawalur.ui.register.RegisterActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private val TAG = "LoginActivity"
+
+    private var mAuth = FirebaseAuth.getInstance()
+    private lateinit var firebaseAuth: FirebaseAuth
+    private val RC_SIGN_IN: Int = 1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +31,65 @@ class LoginActivity : AppCompatActivity() {
 
         btn_daftar.setOnClickListener { startActivity(Intent(this, RegisterActivity::class.java)) }
 
+        btn_masuk.setOnClickListener { login() }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        firebaseAuth = FirebaseAuth.getInstance()
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account)
+                }
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Gagal login : $e", Toast.LENGTH_SHORT).show()
+                Log.e(TAG, "onActivityResult: $e")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                Toast.makeText(this, "Google sign in failed:(", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+
+    private fun login() {
+        val email = edt_email_login.text.toString()
+        val password = edt_password_login.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            this.mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        Toast.makeText(this, "Successfully Logged in :)", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(this, "Error Logging in :(", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        } else {
+            Toast.makeText(this, "Please fill up the Credentials :|", Toast.LENGTH_SHORT).show()
+        }
     }
 }
