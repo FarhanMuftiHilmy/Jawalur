745ca77af8629c5bd9c12af1ce4c733209968f5a
arifudin <nanangarif404@gmail.com>
2020-11-04 16:38:44 +0700

    ubah tema

---
+++ app/src/main/java/com/android/jawalur/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.android.jawalur.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.jawalur.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/android/jawalur/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.android.jawalur.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.jawalur.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
