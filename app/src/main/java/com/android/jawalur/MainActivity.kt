ddc0b57bf22e504adcb313740387a3b11c1bd5bd
arifudin <nanangarif404@gmail.com>
2020-11-04 21:56:54 +0700

    tambah button keluar

--- app/src/main/java/com/android/jawalur/MainActivity.kt
+++ app/src/main/java/com/android/jawalur/MainActivity.kt
@@ -17,7 +17,6 @@ import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
-    val WEB_CLIENT = "564803799642-hd0oai62858tehgclcjmm1kjh8vo3lnj.apps.googleusercontent.com"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,24 +43,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         return false
     }
 
-    private fun keluar() {
-        val firebaseAuth = FirebaseAuth.getInstance()
-        AlertDialog.Builder(this)
-            .setMessage("Apakah Anda yakin ingin keluar?")
-            .setNegativeButton("Tidak", null)
-            .setNeutralButton("Batal", null)
-            .setPositiveButton("Ya") { dialogInterface, i ->
-                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestIdToken(WEB_CLIENT)
-                    .requestEmail()
-                    .build()
-                GoogleSignIn.getClient(this, gso).signOut()
-                firebaseAuth.signOut()
-                startActivity(Intent(this, LoginActivity::class.java))
-                this.finish()
-            }
-            .create().show()
-    }
+
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         var fragment: Fragment? = null
--- app/src/main/java/com/android/jawalur/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/android/jawalur/ui/dashboard/DashboardFragment.kt
@@ -1,33 +1,25 @@
 package com.android.jawalur.ui.dashboard
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.android.jawalur.R
+import com.android.jawalur.ui.login.LoginActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_dashboard.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DashboardFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DashboardFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -36,23 +28,29 @@ class DashboardFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DashboardFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                DashboardFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.img_keluar.setOnClickListener { keluar() }
+    }
+
+    private fun keluar() {
+        val WEB_CLIENT = "564803799642-hd0oai62858tehgclcjmm1kjh8vo3lnj.apps.googleusercontent.com"
+        val firebaseAuth = FirebaseAuth.getInstance()
+        AlertDialog.Builder(context)
+            .setMessage("Apakah Anda yakin ingin keluar?")
+            .setNegativeButton("Tidak", null)
+            .setNeutralButton("Batal", null)
+            .setPositiveButton("Ya") { dialogInterface, i ->
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(WEB_CLIENT)
+                    .requestEmail()
+                    .build()
+                activity?.let { GoogleSignIn.getClient(it, gso).signOut() }
+                firebaseAuth.signOut()
+                startActivity(Intent(context, LoginActivity::class.java))
+                activity?.finish()
+            }
+            .create().show()
     }
 }
